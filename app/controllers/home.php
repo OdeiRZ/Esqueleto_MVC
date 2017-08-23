7f1873ede1be66e5c24b1634a3a7906ff16e3b06
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-08-23 11:41:57 +0200

    Creada estructura de Models

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,8 +2,10 @@
 
 class Home extends Controller
 {
-	public function index($name = '', )
+	public function index($name = '')
 	{
-		
+		$user = $this->model('User');
+		$user->name = $name;
+		echo $user->name;
 	}
 }
