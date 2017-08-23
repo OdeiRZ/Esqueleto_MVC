6af984bbbb1c3a7a647ea8e962cea0a71d2d9c6e
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-08-23 13:05:01 +0200

    Actualizado Controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,15 +1,35 @@
 <?php
 
+/**
+ * Clase Controlador.
+ * Llamado cuando no se ha pasado un método a la aplicación.
+ */
 class Controller
 {
+   /**
+     * Renderiza una vista
+     *
+     * @param string $viewName Nombre de la vista a incluir
+     * @param array  $data Cualquier dato necesario en la vista
+     * @return void
+     */
+	public function view($viewName, $data = [])
+	{
+		// Creamos nueva vista y mostramos contenido
+        $view = new View($viewName, $data);
+		// Hacemos uso de método __toString para mostrar vista
+        echo $view;
+	}
+	
+    /**
+     * Carga un modelo
+     *
+     * @param string $model Nombre de modelo a cargar
+     * @return object
+     */
 	public function model($model)
 	{
 		require_once '../app/models/' . $model . '.php';
 		return new $model();
-	}
-	
-	public function view($view, $data = [])
-	{
-		require_once '../app/views/' . $view . '.php';
 	}
 }
